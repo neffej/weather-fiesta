f51455b28f59a2ca7e1139908332dc5f0c6e6701
neffej <118024182+neffej@users.noreply.github.com>
2023-01-10 20:25:47 -0500

    added clear cities button and functionality. working on display forecast

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,10 @@ var searchItem = {name:'London',state: 'England', country: 'GB'}
 var searchEl = [searchItem.name,searchItem.state,searchItem.country]
 // // var searchString= JSON.stringify(searchEl)
 var searchBtn = document.querySelector('#search-button')
+var clearBtn = document.querySelector('#clear-button')
 var latLongArray=[];
+var savedCoordinates=[];
+var cityList=[];
 
 
 
@@ -32,6 +35,20 @@ var forecastURL= 'http://api.openweathermap.org/data/2.5/forecast?lat=34.8781&lo
 //     var searchInput = document.querySelector('#search').value;
 //     document.getElementById('demo').textContent = searchInput;
 // }
+function init(){
+    var cityList = JSON.parse(localStorage.getItem('citylist'))
+    if(cityList != null){
+    for(var i=0; i < cityList.length; i++){
+        var latLong = cityList[i].latLong
+        var name = cityList[i].cityName
+
+        console.log(name);
+
+        addCityToList(name);
+}
+}
+}
+
 function displaySearchResults(city){
     for(var i=0; i < city.length; i++){
     const list = document.querySelector('#search-results');
@@ -46,7 +63,6 @@ function displaySearchResults(city){
     latLongArray.push({lat, lon})
 
     console.log(latLongArray)
-    
 
     list.appendChild(row);  
 }}
@@ -78,7 +94,16 @@ searchBtn.addEventListener('click', function(event){
         }
 });
 
+clearBtn.addEventListener('click',function(event){
+    event.preventDefault
+    localStorage.clear();
+    // for loop to remove list items from selected cities
+    var cityList = document.querySelector('#selected-cities')
+    while (cityList.firstChild){
+        cityList.removeChild(cityList.firstChild)
+    }
 
+})
 // Select city from list and display weather forecast
 // Select city from list:
 
@@ -87,20 +112,60 @@ document.querySelector('#search-results').addEventListener('click',function(even
     event.preventDefault;
     var city = event.target
     var cityID = Number(city.id)
+    var cityName = city.textContent
     console.log(latLongArray[cityID])
+    var latLong = latLongArray[cityID]
+    
+
+    savedCoordinates.push(latLong);
+    cityList.push({latLong,cityName})
+    localStorage.setItem('citylist',JSON.stringify(cityList));   
+    addCityToList(cityName)
+
+
+
 
     // 'http://api.openweathermap.org/data/2.5/forecast?lat='+city[i].lat+'&lon='+city[i].lon+'&appid=af55055307791ec469a2fe0620680567'
 
 })
 
+function addCityToList(city){
+    // console.log(city.textContent)
+    // Append selected city to list
+    const list = document.querySelector('#selected-cities');
 
-function addCityToList(){
-    const list = document.querySelector('.list-group');
+    const row = document.createElement('a');
 
-    const row = document.createElement('li');
+    row.classList= 'list-item flex-row justify-space-between align-center btn btn-secondary rounded-3 text-info'
+    row.id='saved-city'
+    row.textContent = city
+    
 
-    // row.innerHTML = 
+    list.appendChild(row);  
 }
+document.querySelector('#selected-cities').addEventListener('click',function(event){
+    event.preventDefault;
+    console.log(event.target)
+    var city = event.target
+
+})
+
+
+function displayForecast(city){
+    var geocodingURL= 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid=af55055307791ec469a2fe0620680567'
+
+    fetch(geocodingURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        displaySearchResults(data)
+    })
+}
+
+
+init();
 
 
 
